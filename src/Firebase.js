fbf71f25819f0ac26e04336b25b5b6d29803fe48
alvaro <rekoke92@gmail.com>
2019-02-03 09:41:39 +0800

    first commit, all set up

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'Firebase';
+import DB_KEYS from './keys';
+
+const config = {
+  apiKey: DB_KEYS.API_KEY,
+  authDomain: DB_KEYS.AUTH_DOMAIN,
+  databaseURL: DB_KEYS.DATABASE_URL,
+  projectId: DB_KEYS.PROJECT_ID,
+  storageBucket: DB_KEYS.STORAGE_BUCKET,
+  messagingSenderId: DB_KEYS.MESSAGING_SENDERID
+};
+firebase.initializeApp(config);
+
+export default firebase;
